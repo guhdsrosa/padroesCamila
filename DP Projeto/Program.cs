3f513b68defcb3e5ee61b56022365fef3ee20f5d
Gustavo da Silva Rosa <65796235+guhdsrosa@users.noreply.github.com>
2023-04-20 01:46:18 -0300

    Commit 20-04

---
+++ DP Projeto/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+interface OperacaoMatematica
+{
+    int Calcular(int x, int y);
+}
+
+class Adicao : OperacaoMatematica
+{
+    public int Calcular(int x, int y)
+    {
+        return x + y;
+    }
+}
+
+class Subtracao : OperacaoMatematica
+{
+    public int Calcular(int x, int y)
+    {
+        return x - y;
+    }
+}
+
+class Divisao : OperacaoMatematica
+{
+    public int Calcular(int x, int y)
+    {
+        return x / y;
+    }
+}
+
+class Multiplicacao : OperacaoMatematica
+{
+    public int Calcular(int x, int y)
+    {
+        return x * y;
+    }
+}
+
+class MateContext
+{
+    private OperacaoMatematica _operacaoMate;
+
+    public MateContext(OperacaoMatematica operacaoMate)
+    {
+        this._operacaoMate = operacaoMate;
+    }
+
+    public void ExecuteoperacaoMate(int x, int y)
+    {
+        int result = this._operacaoMate.Calcular(x, y);
+        Console.WriteLine($"Resultado: {result}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MateContext MateContext = new MateContext(new Adicao());
+        MateContext.ExecuteoperacaoMate(10, 5);
+
+        MateContext = new MateContext(new Multiplicacao());
+        MateContext.ExecuteoperacaoMate(10, 5);
+
+        MateContext = new MateContext(new Divisao());
+        MateContext.ExecuteoperacaoMate(10, 5);
+
+        MateContext = new MateContext(new Subtracao());
+        MateContext.ExecuteoperacaoMate(10, 5);
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Singleton
+{
+    private static Singleton instance;
+
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+
+    public void PrintMessage()
+    {
+        Console.WriteLine("Olá mundo!");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Singleton t1 = Singleton.Instance;
+        Singleton t2 = Singleton.Instance;
+
+        t1.PrintMessage();
+        t2.PrintMessage();
+
+        Console.ReadLine();
+    }
+}
